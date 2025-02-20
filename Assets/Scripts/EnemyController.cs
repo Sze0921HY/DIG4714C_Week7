7b9b8a3a618f875ece15b26a2c85aa6179bde0db
Sze <sz982600@ucf.edu>
2025-02-20 16:42:21 -0500

    enemy chasing

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,21 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 public class EnemyBehavior : MonoBehaviour
 {
+    [SerializeField]
+    private Transform[] waypoints;
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    public GameObject player;
+    private bool foundPlayer;
+    private int currentWaypointIndex;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.Find("Player");
+        foundPlayer = false;
+        currentWaypointIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (foundPlayer)
+        {
+            navMeshAgent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.1f)
+            {
+                // Move to the next waypoint
+                currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+                navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("collide");
-
         if (other.name == "Player")
         {
+            foundPlayer = true;
             Debug.Log("Player detected - attack!");
         }
     }
+
     void OnTriggerExit(Collider other)
     {
         if (other.name == "Player")
         {
+            foundPlayer = false;
             Debug.Log("Player out of range, resume patrol");
         }
     }
